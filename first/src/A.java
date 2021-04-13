930b94ef5d195f21825cc4a4e639f18f1b3138cb
2170987947 <2170987947@qq.com>
2021-04-13 09:46:57 +0800

    数据库连接

--- first/src/A.java
+++ first/src/A.java
@@ -3,9 +3,8 @@ import java.sql.*;
 public class A {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.jdbc.Driver");
-        Connection connection = DriverManager.getConnection("jdbc:mysql: /localhost: 3306/test", "root", "123456");
+        Connection connection = DriverManager.getConnection("jdbc:mysql: /localhost: 3306/blogdemo", "root", "123456");
         Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from stud");
         System.out.println(connection);
     }
 }
