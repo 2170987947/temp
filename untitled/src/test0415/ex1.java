63851a2fa65201936a3a6ba173683989350d02a8
2170987947 <2170987947@qq.com>
2021-04-15 18:50:52 +0800

    算法练习2

---
+++ untitled/src/test0415/ex1.java
@@ -0,0 +1,15 @@
+package test0415;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/4/15 13:11
+ */
+public class ex1 {
+    private static final String ss = "D:\\Program Files\\Everything";
+    private static final String API_PATH = "D:\\jdkAPI\\jdk-8u281-docs-all\\docs\\api";
+
+    public static void main(String[] args) {
+        System.out.println(ss.substring(10));
+    }
+}
