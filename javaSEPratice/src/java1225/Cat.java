1a13e3e97e6c86b8f84654c8680da8609ad7c407
2170987947 <2170987947@qq.com>
2021-04-21 21:09:00 +0800

    练习

---
+++ javaSEPratice/src/java1225/Cat.java
@@ -0,0 +1,13 @@
+package java1225;
+
+ class Cat {
+          String name;
+         int age;
+         String breed;
+          void eat(String food) {
+            System.out.println(name + "正在吃" + food);
+        }
+         void run() {
+            System.out.println(name + "正在跑");
+        }
+}
