f1126ed2ecc875939b7d0c5deed98645b68717ad
2170987947 <2170987947@qq.com>
2021-04-30 23:58:12 +0800

    算法练习19

---
+++ untitled/src/test0430/ex1.java
@@ -0,0 +1,12 @@
+package test0430;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/4/30 23:57
+ */
+public class ex1 {
+    public static void main(String[] args) {
+        System.out.println("jkl");
+    }
+}
