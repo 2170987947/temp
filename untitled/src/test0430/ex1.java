3338670232a60d50d0674b25a087a7f501b4a58c
2170987947 <2170987947@qq.com>
2021-05-03 22:10:20 +0800

    算法练习20

--- untitled/src/test0430/ex1.java
+++ untitled/src/test0430/ex1.java
@@ -1,5 +1,7 @@
 package test0430;
 
+import java.util.Scanner;
+
 /**
  * @author FMM
  * @version 7.0
@@ -8,6 +10,12 @@ package test0430;
 public class ex1 {
     public static void main(String[] args) {
         System.out.println("jkl");
-        
+        String str = "www";
+        Scanner in = new Scanner(System.in);
+
+        int i = in.nextInt();
+        int j = in.nextInt();
+        System.out.println(i + j);
+//        System.out.println(str.concat("Hello"));
     }
 }
