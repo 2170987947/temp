e8ca5b60d97b4e258fd718ce2b05940dcca30bbd
2170987947 <2170987947@qq.com>
2021-05-12 12:20:32 +0800

    GUI界面1

---
+++ interface-gui/src/test/java/org/example/ResultTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.Test;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/5/12 12:13
+ */
+public class ResultTest {
+    @Test
+    public void main() {
+        new org.example.Test();
+    }
+}
