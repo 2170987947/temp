87deabf14dba8129a2c037705e22fa8b12a48697
2170987947 <2170987947@qq.com>
2021-05-14 00:08:34 +0800

    Apriori算法

---
+++ apriori/src/main/java/org/example/Client.java
@@ -0,0 +1,19 @@
+package org.example;
+
+/**
+ * @author FMM
+ * @version 7.0
+ * @date 2021/5/13 10:32
+ */
+
+/**
+ * apriori关联规则挖掘算法调用类
+ */
+public class Client {
+    public static void main(String[] args){
+        String filePath = "D:\\Gitku\\testInput.txt";
+        AprioriTool tool = new AprioriTool(filePath, 2);
+        tool.printAttachRule(0.7);
+    }
+}
+
