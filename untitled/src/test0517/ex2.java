ee6f767236e53bb298a2ba1cef050d61d87caaad
fmm <2170987947@qq.com>
2021-05-17 11:02:37 +0800

    算法练习26

--- untitled/src/test0517/ex2.java
+++ untitled/src/test0517/ex2.java
@@ -1,4 +1,33 @@
 package test0517;
 
+import java.util.Scanner;
+
 public class ex2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLong()) {
+            long num = in.nextLong();
+            System.out.println(root(num));
+        }
+    }
+
+    private static long root(long num) {
+        if (num < 10) {
+            return num;
+        }
+        long ret = 0;
+        while (true) {
+            while (num >= 10) {
+                ret += num % 10;
+                num /= 10;
+            }
+            ret += num;
+            if (ret < 10) {
+                return ret;
+            } else {
+                num = ret;
+                ret = 0;
+            }
+        }
+    }
 }
