7cc7edf61b61dd7402815d4eab9508730765b5e3
fmm <2170987947@qq.com>
2021-05-19 11:02:22 +0800

    算法练习26

--- untitled/src/test0515/ex3.java
+++ untitled/src/test0515/ex3.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class ex3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        while (in.hasNext()) {
-        }
+        int[][] arr = {{426, 306, 641, 372, 477, 409},
+                {223, 172, 327, 586, 363, 553},
+                {291, 645, 248, 316, 711, 295},
+                {127, 192, 495, 208, 547, 175},
+                {131, 448, 178, 264, 207, 676},
+                {655, 407, 309, 358, 246, 714}
+        };
+        System.out.println(getMost(arr));
     }
 
-    public int getMost(int[][] board) {
+    public static int getMost(int[][] board) {
         // write code here
         for (int j = 1; j < board[0].length; j++) {
             board[0][j] += board[0][j - 1];
@@ -19,10 +25,10 @@ public class ex3 {
         }
         for (int i = 1; i < board.length; i++) {
             for (int j = 1; j < board[0].length; j++) {
-                board[i][j] = Math.max(board[i][j - 1], board[i - 1][j]);
+                board[i][j] += Math.max(board[i][j - 1], board[i - 1][j]);
             }
         }
-        return board[5][5];
+        return board[board.length - 1][board[0].length - 1];
     }
 
 }
