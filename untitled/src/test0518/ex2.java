61032cc0cf8ed2a5c63adecb3f69fb43b49f4ed2
fmm <2170987947@qq.com>
2021-05-21 14:48:56 +0800

    算法练习27

--- untitled/src/test0518/ex2.java
+++ untitled/src/test0518/ex2.java
@@ -1,23 +1,17 @@
 package test0518;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 
 public class ex2 {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println(Integer.MAX_VALUE);
         while (in.hasNextInt()) {
-            String str = in.nextLine();
-            String[] p = str.split(" ");
-
-            int n = Integer.parseInt(p[0]);
-            int r = Integer.parseInt(p[1]);
-            if (2 * 3.14 * r < n) {
-                System.out.println("No");
-            } else {
-                System.out.println("Yes");
-            }
+            BigDecimal n = new BigDecimal(in.next());
+            BigDecimal r = new BigDecimal(in.next());
+            BigDecimal t = new BigDecimal("6.28");
+            System.out.println((r.multiply(t)).compareTo(n) >= 0 ? "No" : "Yes" );
         }
     }
 
--- untitled/src/test0520/ex1.java
+++ untitled/src/test0520/ex1.java
@@ -1,5 +1,6 @@
 package test0520;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Scanner;
@@ -8,30 +9,15 @@ public class ex1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         while (in.hasNext()) {
-            String a = in.next();
-            String b = in.next();
-            String c = in.next();
-            if (isSanJiaoXing(a, b, c)) {
+            BigDecimal a = in.nextBigDecimal();
+            BigDecimal b = in.nextBigDecimal();
+            BigDecimal c = in.nextBigDecimal();
+
+            if (a.add(b).compareTo(c) > 0 && a.add(c).compareTo(b) > 0 && b.add(c).compareTo(a) > 0) {
                 System.out.println("Yes");
             } else {
                 System.out.println("No");
             }
         }
     }
-
-    private static boolean isSanJiaoXing(String a, String b, String c) {
-        String[] tep = new String[]{a, b, c};
-        Arrays.sort(tep);
-        BigInteger tmp1 = new BigInteger(tep[0]);
-        BigInteger tmp2 = new BigInteger(tep[1]);
-        BigInteger tmp3 = new BigInteger(tep[2]);
-        System.out.println(tep[0]);
-        System.out.println(tep[1]);
-        System.out.println(tep[2]);
-        if (tmp1.add(tmp2).compareTo(tmp3) > 0) {
-            return true;
-        }
-        return false;
-    }
-
 }
