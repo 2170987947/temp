7574ccc58175aed1f24cb943526a9b22d5223c0a
fmm <2170987947@qq.com>
2021-05-22 15:35:46 +0800

    算法练习29

---
+++ untitled/src/test0522/ex1.java
@@ -0,0 +1,32 @@
+package test0522;
+
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args) {
+        Character[] characters = new Character[26];
+        characters[0] = 'A';
+        for (int i = 1; i < characters.length; i++) {
+            characters[i] = (char)(characters[i - 1] + 1);
+        }
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextLine()) {
+            String a = in.nextLine();
+            jiema(a);
+        }
+    }
+
+    private static void jiema(String a) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) >= 'A' && a.charAt(i) < 'F') {
+                stringBuffer.append((char)('Z' - (5 - (a.charAt(i) - 'A')) + 1));
+            } else if (a.charAt(i) >= 'F' && a.charAt(i) <= 'Z') {
+                stringBuffer.append((char)(a.charAt(i) - 5));
+            } else {
+                stringBuffer.append((char)a.charAt(i));
+            }
+        }
+        System.out.println(stringBuffer);
+    }
+}
