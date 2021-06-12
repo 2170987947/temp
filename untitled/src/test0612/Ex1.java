c2e5a9c6d15620ea050813404ac5efe4b9186d69
fmm <2170987947@qq.com>
2021-06-12 22:02:12 +0800

    算法练习41

---
+++ untitled/src/test0612/Ex1.java
@@ -0,0 +1,12 @@
+package test0612;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNext()) {
+
+        }
+    }
+}
