1ecb4d246c7690e17968cc095166f541ac01a643
fmm <2170987947@qq.com>
2021-06-17 18:14:46 +0800

    算法练习44

--- untitled/src/test0613/Ex2.java
+++ untitled/src/test0613/Ex2.java
@@ -3,21 +3,36 @@ package test0613;
 import java.util.Arrays;
 
 public class Ex2 {
-    private static int[] yuan(int[][] mat, int n , int m, int start) {
-        int[] t = new int[1];
-        return t;
-    }
+
     public int[] clockwisePrint(int[][] mat, int n, int m) {
         // write code here
         int[] r = new int[n * m];
-        int col = 0;
-        int row = 0;
-        while (true) {
-            if (n <= 0 || m <= 0) {
-                break;
+        int x = 0;
+        int y = 0;
+        int row = n - 1;
+        int col = m - 1;
+        int index = 0;
+        while (x <= row && y <= col) {
+            for (int i = y; i <= col; i++) {
+                r[index++] = mat[x][i];
             }
-
-            int[] a = yuan(mat, n, m, 0);
+            for (int i = x + 1; i <= row; i++) {
+                r[index++] = mat[i][col];
+            }
+            if (x < row) {
+                for (int i = col - 1; i >= y; i++) {
+                    r[index++] = mat[row][i];
+                }
+            }
+            if (y < col) {
+                for (int i = row - 1; i > x; i++) {
+                    r[index++] = mat[i][y];
+                }
+            }
+            x++;
+            y++;
+            row--;
+            col--;
         }
         return r;
     }
