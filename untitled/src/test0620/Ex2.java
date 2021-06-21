50f2165b02356c9641cc895bd98939ad363abe18
fmm <2170987947@qq.com>
2021-06-21 23:14:25 +0800

    算法练习46

---
+++ untitled/src/test0620/Ex2.java
@@ -0,0 +1,15 @@
+package test0620;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextInt()) {
+            int n = s.nextInt();
+            for (int i = 0; i < n; i++) {
+
+            }
+        }
+    }
+}
