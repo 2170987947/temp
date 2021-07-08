9aac82d49a84d816a9c1175be51063e58f9dc684
fmm <2170987947@qq.com>
2021-07-08 22:43:02 +0800

    练习

---
+++ jvm-study/src/main/java/org/example/clone/Student.java
@@ -0,0 +1,48 @@
+
+package org.example.clone;
+
+class Student implements Cloneable {
+    private String name;
+    private int age;
+    private Teacher teacher;
+
+    public Student clone() {
+        Student student = null;
+        try {
+            student = (Student) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return student;
+    }
+
+    public Student(String name, int age, Teacher teacher) {
+        this.name = name;
+        this.age = age;
+        this.teacher = teacher;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+}
