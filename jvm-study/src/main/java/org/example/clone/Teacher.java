cb5bf414b995b7ae2212fcd5e2f8f0d100ab8899
fmm <2170987947@qq.com>
2021-07-14 23:16:39 +0800

    练习

---
+++ jvm-study/src/main/java/org/example/clone/Teacher.java
@@ -0,0 +1,27 @@
+package org.example.clone;
+
+class Teacher {
+    private String name;
+    private String direction;
+
+    public Teacher(String name, String direction) {
+        this.name = name;
+        this.direction = direction;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+}
