65ecd495b11f6cfc52b49d817d57dae92e177b25
fmm <2170987947@qq.com>
2021-08-17 23:26:46 +0800

    练习

---
+++ java-study/src/main/java/org/example/java21_0817/Ex1.java
@@ -0,0 +1,11 @@
+package org.example.java21_0817;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String p = s.nextLine();
+        String[] pp = p.split(" ");
+    }
+}
