c5eca00635d4d8c28dce0d6658a3244bad15281d
fmm <2170987947@qq.com>
2021-08-21 23:42:58 +0800

    练习

---
+++ java-study/src/main/java/org/example/java21_0821/Ex1.java
@@ -0,0 +1,9 @@
+package org.example.java21_0821;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+    }
+}
