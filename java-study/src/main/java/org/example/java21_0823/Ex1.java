b7345dba91deb467a712b2a232511a83f6f0ffaf
fmm <2170987947@qq.com>
2021-08-23 22:21:52 +0800

    练习

--- java-study/src/main/java/org/example/java21_0823/Ex1.java
+++ java-study/src/main/java/org/example/java21_0823/Ex1.java
@@ -5,5 +5,9 @@ import java.util.Scanner;
 public class Ex1 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) {
+            System.out.println(i);
+        }
     }
 }
