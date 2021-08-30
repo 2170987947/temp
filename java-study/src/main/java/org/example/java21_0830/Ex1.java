6c4f6f842b600157007598494b4d30f16cf2704a
2170987947 <2170987947@qq.com>
2021-08-30 18:56:30 +0800

    练习

---
+++ java-study/src/main/java/org/example/java21_0830/Ex1.java
@@ -0,0 +1,18 @@
+package org.example.java21_0830;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int one = 0;
+        int two = 1;
+        while (two < n) {
+            int three = one + two;
+            one = two;
+            two = three;
+        }
+        System.out.println(Math.min(n - one, two - n));
+    }
+}
