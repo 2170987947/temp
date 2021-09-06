b3dbb4f86b0c9673901ad964dd4944f5fe18e5c5
2170987947 <2170987947@qq.com>
2021-09-06 22:59:26 +0800

    练习

---
+++ java-study/src/main/java/org/example/java21_0906/Ex1.java
@@ -0,0 +1,28 @@
+package org.example.java21_0906;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        StringBuffer r = new StringBuffer();
+        String s = "1";
+        String t = "99";
+        int i = s.length() - 1;
+        int j = t.length() - 1;
+        int jin = 0;
+        while (i >= 0 || j >= 0) {
+            int a = (i >= 0 ? s.charAt(i--) - '0' : 0);
+            int b = (j >= 0 ? t.charAt(j--) - '0' : 0);
+            int c = a + b + jin;
+            if (c > 9) {
+                jin = c / 10;
+                c = c % 10;
+            } else {
+                jin = 0;
+            }
+            r.append(c);
+        }
+        if (jin > 0) {
+            r.append(jin);
+        }
+        System.out.println(r.reverse().toString());
+    }
+}
