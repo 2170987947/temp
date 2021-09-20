1a053aac82d774132e5a9cc480553a77e2a1afdf
2170987947 <2170987947@qq.com>
2021-09-20 23:33:50 +0800

    练习

--- java-study/src/main/java/org/example/java21_0918/Ex2.java
+++ java-study/src/main/java/org/example/java21_0918/Ex2.java
@@ -4,6 +4,7 @@ import java.sql.CallableStatement;
 
 public class Ex2 {
     public static void main(String[] args) {
-        System.out.println("jghnk");
+        System.out.println("jg");
+        System.out.println(Math.pow(4, 2));
     }
 }
