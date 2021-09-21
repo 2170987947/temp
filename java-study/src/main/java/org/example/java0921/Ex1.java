1ee82f9648143c6f30cc03e6339665612ad6493e
2170987947 <2170987947@qq.com>
2021-09-21 15:00:36 +0800

    练习

---
+++ java-study/src/main/java/org/example/java0921/Ex1.java
@@ -0,0 +1,10 @@
+package org.example.java0921;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        int i = 5;
+        int y = i >> 2;
+
+        System.out.println(y >>> 2);
+    }
+}
