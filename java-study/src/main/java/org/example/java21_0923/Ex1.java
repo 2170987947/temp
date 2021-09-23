a3fee0e77d72983b6d8590a0341dd3d97fa5a1ff
2170987947 <2170987947@qq.com>
2021-09-24 01:39:25 +0800

    练习

--- java-study/src/main/java/org/example/java21_0923/Ex1.java
+++ java-study/src/main/java/org/example/java21_0923/Ex1.java
@@ -4,5 +4,6 @@ public class Ex1 {
     public static void main(String[] args) {
         System.out.printf("okdhc");
         System.out.printf("oeidjhb");
+
     }
 }
---
+++ java-study/src/main/java/org/example/java21_0924/Ex1.java
@@ -0,0 +1,7 @@
+package org.example.java21_0924;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        //
+    }
+}
