3875995cbaba81b567375349e45311be0f4b6742
2170987947 <2170987947@qq.com>
2021-09-24 02:04:33 +0800

    练习

--- java-study/src/main/java/org/example/java21_0924/Ex1.java
+++ java-study/src/main/java/org/example/java21_0924/Ex1.java
@@ -1,7 +1,10 @@
 package org.example.java21_0924;
 
+import java.util.Scanner;
+
 public class Ex1 {
     public static void main(String[] args) {
         //
+        Scanner s = new Scanner(System.in);
     }
 }
