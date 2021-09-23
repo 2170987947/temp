7921ceec6ca8a568bb68fc7f040851d1161ae124
2170987947 <2170987947@qq.com>
2021-09-23 12:03:13 +0800

    练习

--- java-study/src/main/java/org/example/java21_0923/Ex1.java
+++ java-study/src/main/java/org/example/java21_0923/Ex1.java
@@ -3,5 +3,6 @@ package org.example.java21_0923;
 public class Ex1 {
     public static void main(String[] args) {
         System.out.printf("okdhc");
+        System.out.printf("oeidjhb");
     }
 }
