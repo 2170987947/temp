abbd7490113ecefe7918f10f38f566873d084b26
2170987947 <2170987947@qq.com>
2021-09-27 19:18:25 +0800

    练习

--- java-study/src/main/java/org/example/java21_0924/Ex1.java
+++ java-study/src/main/java/org/example/java21_0924/Ex1.java
@@ -6,18 +6,9 @@ import java.util.Scanner;
 
 public class Ex1 {
     public static void main(String[] args) {
-        //
         Map<Integer, Integer> map = new HashMap<>();
-        for (Map.Entry e :
-             map.entrySet()) {
-
-        }
         Scanner s = new Scanner(System.in);
         while (s.hasNext()) {
-            System.out.printf("");
-
-
-
         }
     }
 }
