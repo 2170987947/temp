984d75a849c46bc5244af6e2d4a9c7dedfc189a9
2170987947 <2170987947@qq.com>
2021-09-28 21:38:46 +0800

    练习

---
+++ java-study/src/main/java/org/example/java21_0928/Ex4.java
@@ -0,0 +1,9 @@
+package org.example.java21_0928;
+
+public class Ex4 {
+    public static void main(String[] args) {
+        String str = "dshcujne c26 789cd snj";
+        String a = str.replace(" ", "insert");
+        System.out.println(a);
+    }
+}
