edb7f309b400ef7d89e2665b2aa31e037acbd99e
2170987947 <2170987947@qq.com>
2021-09-30 10:41:33 +0800

    练习

---
+++ java-study/src/main/java/org/example/java21_0929/Ex2.java
@@ -0,0 +1,16 @@
+package org.example.java21_0929;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextInt()) {
+            int n = s.nextInt();
+            int sum = 0;
+            while (n != 0) {
+
+            }
+        }
+    }
+}
