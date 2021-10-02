1360f9c0cf78293a671252ee44fc165820bcdb30
2170987947 <2170987947@qq.com>
2021-10-02 12:05:30 +0800

    练习

--- java-study/src/main/java/org/example/java21_1002/Ex1.java
+++ java-study/src/main/java/org/example/java21_1002/Ex1.java
@@ -1,7 +1,12 @@
 package org.example.java21_1002;
 
+import java.util.Scanner;
+
 public class Ex1 {
     public static void main(String[] args) {
-        
+        Scanner s = new Scanner(System.in);
+        while (s.hasNextInt()) {
+            
+        }
     }
 }
