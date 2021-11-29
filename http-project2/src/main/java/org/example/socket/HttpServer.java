7bfa608e65fefaf57e3b0aee370cc5937a6ac780
2170987947 <2170987947@qq.com>
2021-11-29 18:02:06 +0800

    Http服务器

--- http-project2/src/main/java/org/example/socket/HttpServer.java
+++ http-project2/src/main/java/org/example/socket/HttpServer.java
@@ -3,15 +3,21 @@ package org.example.socket;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HttpServer {
     public static void main(String[] args) throws IOException, InterruptedException {
+        // 基本都是队列长度无限长，如果任务处理的慢，会导致都堆积在队列中，
+        // 而对队列在内存中，会导致内存被消耗完
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
+
         ServerSocket serverSocket = new ServerSocket(8080);
 
         while (true) {
             Socket accept = serverSocket.accept();
             Runnable task = new RequestResponseTask(accept);
-            new Thread(task).start();
+            threadPool.execute(task);
         }
 
     }
--- http-project2/src/main/java/org/example/socket/RequestResponseTask.java
+++ http-project2/src/main/java/org/example/socket/RequestResponseTask.java
@@ -2,9 +2,20 @@ package org.example.socket;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
 public class RequestResponseTask implements Runnable {
+    private static final String DOC_BASH = "D:\\Git\\temp\\http-project2";
     private final Socket accept;
+    private static final Map<String, String> mimeTypeMap = new HashMap<>();
+
+    static {
+        mimeTypeMap.put("txt", "text/plain");
+        mimeTypeMap.put("html", "text/html");
+        mimeTypeMap.put("js", "application/javascript");
+    }
 
     public RequestResponseTask(Socket socket) {
         accept = socket;
@@ -12,27 +23,67 @@ public class RequestResponseTask implements Runnable {
 
     public void run() {
         try {
-            OutputStream outputStream = accept.getOutputStream();
-            Writer writer = new OutputStreamWriter(outputStream, "UTF-8");
-            PrintWriter printWriter = new PrintWriter(writer);
+            InputStream inputStream = accept.getInputStream();
+            Scanner scanner = new Scanner(inputStream, "UTF-8");
+            scanner.next();
+            String path = scanner.next();
+            String filePath = DOC_BASH + path;
+            // 1. 判断该文件是否存在
+            File resourse = new File(filePath);
+            if (resourse.exists()) {
+                OutputStream outputStream = accept.getOutputStream();
+                Writer writer = new OutputStreamWriter(outputStream, "UTF-8");
+                PrintWriter printWriter = new PrintWriter(writer);
 
-            Thread.sleep(10000);
-            // 谐响应
-            // 写响应行
-            printWriter.println("HTTP/1.0 200 OK");
-            // 写响应头
-            printWriter.println("Content-Type: text/html; charset=utf-8\r");
-            // 空行
-            printWriter.println();
-            // 写响应体， html内容
-            printWriter.println("<h1>我是服务器，给你消息<h1>");
-            printWriter.flush();
+                String contentType = "text/plain";
+                if (path.contains(".")) {
+                    int i = path.lastIndexOf(".");
+                    String substring = path.substring(i + 1);
+                    contentType = mimeTypeMap.getOrDefault(substring, contentType);
+                }
+                if (contentType.startsWith("text/")) {
+                    contentType = contentType + ";charset=utf-8";
+                }
+                // 谐响应
+                // 写响应行
+                printWriter.println("HTTP/1.0 200 OK");
+                // 写响应头
+                printWriter.printf("Content-Type: %s\n", contentType);
+                // 空行
+                printWriter.println();
+
+                try (InputStream resourseInputStream = new FileInputStream(resourse)) {
+                    byte[] buffer = new byte[1024];
+                    while (true) {
+                        int len = resourseInputStream.read(buffer);
+                        if (len == -1) {
+                            break;
+                        }
+                        outputStream.write(buffer, 0, len);
+                    }
+                    outputStream.flush();
+                }
+
+                // 写响应体， html内容
+                printWriter.println("<h1>我是服务器，给你消息<h1>");
+                printWriter.flush();
+            } else {
+                OutputStream outputStream = accept.getOutputStream();
+                Writer writer = new OutputStreamWriter(outputStream, "UTF-8");
+                PrintWriter printWriter = new PrintWriter(writer);
+
+                printWriter.println("HTTP/1.0 400 Not Found");
+                printWriter.println("Content-Type: text/html;charset=utf-8");
+                printWriter.println();
+                printWriter.printf("<h1>%s 找不到资源\n", path);
+
+                printWriter.flush();
+
+            }
 
         } catch (IOException e) {
             // 因为单次请求响应周期错误，不影响其他的
             e.printStackTrace(System.out);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         } finally {
             try {
                 accept.close();
