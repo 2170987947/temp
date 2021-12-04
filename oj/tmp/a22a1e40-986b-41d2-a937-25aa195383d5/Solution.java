ed65247ab1f55ccbf54e5c8ff55aa9c663d80c6f
2170987947 <2170987947@qq.com>
2021-12-04 19:54:28 +0800

    在线OJ

---
+++ oj/tmp/a22a1e40-986b-41d2-a937-25aa195383d5/Solution.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public int addDigits(int num) {
+        int r = num;
+        while (r > 10) {
+            num = r;
+            r = 0;
+            while (num > 0) {
+                r += num % 10;
+                num /= 10;
+            }
+        }
+        return r;
+    }
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        if (s.addDigits(38) == 2) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("test failed");
+        }
+
+        if (s.addDigits(1) == 1) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("test failed");
+        }
+    }
+}
